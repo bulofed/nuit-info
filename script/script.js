073213f38467c320f9f2e2ffda4cf3766ac42fc5
Fyrosk <97893455+Fyrosk@users.noreply.github.com>
2022-12-02 03:04:32 +0100

    changement sur les boutons pour choisir les jeux
    boutons passer de liens à "button" en html

--- script/script.js
+++ script/script.js
@@ -1,12 +1,2 @@
 var element = document.getElementById("secret");
 const easterEgg = new Konami(() => element.classList.add("secret"));
-
-$(document).ready(function() {    
-    $(".button-start").click(function() {
-        if ($("#Accueil").hasClass("active"))
-        {
-            $("#Accueil").removeClass("active").addClass("inactive");
-            $("#choix").removeClass("inactive").addClass("active");
-        }
-    });
-});
