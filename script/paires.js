c6e36b727099b0a97d6c52be1de221e6e334b91b
AkumaCipher <petitmamass59@gmail.com>
2022-12-02 06:50:46 +0100

    Face affichables

--- script/paires.js
+++ script/paires.js
@@ -1,3 +1,85 @@
 $(document).ready(function () {
+  $("#carte-1").click(function () {
+    if ($("#carte-1").hasClass("carte")) {
+      $("#carte-1").removeClass("carte").addClass("carte-invisible");
+      $("#face-1").removeClass("face").addClass("face-visible");
+    }
+  });
+
+  $("#carte-2").click(function () {
+    if ($("#carte-2").hasClass("carte")) {
+      $("#carte-2").removeClass("carte").addClass("carte-invisible");
+      $("#face-2").removeClass("face").addClass("face-visible");
+    }
+  });
     
-})
+    $("#carte-3").click(function () {
+      if ($("#carte-3").hasClass("carte")) {
+        $("#carte-3").removeClass("carte").addClass("carte-invisible");
+        $("#face-3").removeClass("face").addClass("face-visible");
+      }
+    });
+
+    $("#carte-4").click(function () {
+      if ($("#carte-4").hasClass("carte")) {
+        $("#carte-4").removeClass("carte").addClass("carte-invisible");
+        $("#face-4").removeClass("face").addClass("face-visible");
+      }
+    });
+
+    $("#carte-5").click(function () {
+      if ($("#carte-5").hasClass("carte")) {
+        $("#carte-5").removeClass("carte").addClass("carte-invisible");
+        $("#face-5").removeClass("face").addClass("face-visible");
+      }
+    });
+
+    $("#carte-6").click(function () {
+      if ($("#carte-6").hasClass("carte")) {
+        $("#carte-6").removeClass("carte").addClass("carte-invisible");
+        $("#face-6").removeClass("face").addClass("face-visible");
+      }
+    });
+
+    $("#carte-7").click(function () {
+      if ($("#carte-7").hasClass("carte")) {
+        $("#carte-7").removeClass("carte").addClass("carte-invisible");
+        $("#face-7").removeClass("face").addClass("face-visible");
+      }
+    });
+
+    $("#carte-8").click(function () {
+      if ($("#carte-8").hasClass("carte")) {
+        $("#carte-8").removeClass("carte").addClass("carte-invisible");
+        $("#face-8").removeClass("face").addClass("face-visible");
+      }
+    });
+
+    $("#carte-9").click(function () {
+      if ($("#carte-9").hasClass("carte")) {
+        $("#carte-9").removeClass("carte").addClass("carte-invisible");
+        $("#face-9").removeClass("face").addClass("face-visible");
+      }
+    });
+
+    $("#carte-10").click(function () {
+      if ($("#carte-10").hasClass("carte")) {
+        $("#carte-10").removeClass("carte").addClass("carte-invisible");
+        $("#face-10").removeClass("face").addClass("face-visible");
+      }
+    });
+
+    $("#carte-11").click(function () {
+      if ($("#carte-11").hasClass("carte")) {
+        $("#carte-11").removeClass("carte").addClass("carte-invisible");
+        $("#face-11").removeClass("face").addClass("face-visible");
+      }
+    });
+
+    $("#carte-12").click(function () {
+      if ($("#carte-12").hasClass("carte")) {
+        $("#carte-12").removeClass("carte").addClass("carte-invisible");
+        $("#face-12").removeClass("face").addClass("face-visible");
+      }
+    });
+});
