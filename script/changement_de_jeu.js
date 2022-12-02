3797cb1e1c090962c6427a77e0f787a1ce194e47
Fyrosk <97893455+Fyrosk@users.noreply.github.com>
2022-12-02 03:09:10 +0100

    iyfad

--- script/changement_de_jeu.js
+++ script/changement_de_jeu.js
@@ -6,4 +6,8 @@ $(document).ready(function() {
             $("#choix").removeClass("inactive").addClass("active");
         }
     });
+
+    $(".button_paires").click(function(){
+        
+    })
 });
